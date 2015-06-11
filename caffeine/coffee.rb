c81ff23d9ec40c0db5ca78dffd563dcd94bf2044
Joey Poon <joey@joeypoon.com>
2015-06-11 10:07:30 -0500

    day-29 challenge

--- caffeine/coffee.rb
+++ caffeine/coffee.rb
@@ -1,3 +1,18 @@
 class Coffee
-  # Add code here
+  attr_accessor :level
+  attr_reader :coffee
+
+  def initialize coffee
+    @coffee = coffee
+    @level = 1
+  end
+
+  def full?
+    level == 1
+  end
+
+  def empty?
+    level == 0
+  end
+
 end
--- caffeine/human.rb
+++ caffeine/human.rb
@@ -1,3 +1,29 @@
 class Human
-  # Add code here
+  attr_reader :name, :alertness, :coffee
+
+  def initialize name
+    @name = name
+    @alertness = 0
+  end
+
+  def has_coffee?
+    alertness > 0.1
+  end
+
+  def needs_coffee?
+    alertness < 0.1
+  end
+
+  def buy coffee
+    @coffee = coffee
+  end
+
+  def drink!
+    @coffee.level -= 0.34
+    if @coffee.level < 0
+      @coffee.level = 0
+    end
+    @alertness += 0.33
+  end
+
 end
